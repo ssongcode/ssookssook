1c31cd052b60a0c1e468241d6bbbab04ca811b0a
dark6ro <dark6ro@naver.com>
2023-07-19 15:55:00 +0900

    feat: Arduino : 아두이노 기능 테스트

--- IoT/ArduinoTest/ArduinoTest.ino
+++ IoT/ArduinoTest/ArduinoTest.ino
@@ -1,16 +1,31 @@
-#include <DFRobot_DHT11.h> // 이 라이브러리를 사용하겠다. 
-DFRobot_DHT11 DHT;
-#define DHT11_PIN A0 // 아두이노 핀 10번으로 정의하겠다. (변경가능)
+#include <DFRobot_DHT11.h> // 온습도 센서 라이브러리 
+DFRobot_DHT11 DHT; // 온습도 센서 객체 
+#define PIN A0 // 아두이노 핀은 A0를 사용
 
 void setup(){
-  Serial.begin(9600); //(컴퓨터와 통신을 115200속도로 하겠다.)
+  Serial.begin(9600);
 }
 
 void loop(){
-  DHT.read(DHT11_PIN);
+  // 온습도 센서
+  DHT.read(PIN);
   Serial.print("temp:");
-  Serial.print(DHT.temperature); //온도를 출력하겠다.
+  Serial.print(DHT.temperature); // 온도 출력
   Serial.print("  humi:");
-  Serial.println(DHT.humidity); //습도를 출력하겠다.
-  delay(1000);
+  Serial.println(DHT.humidity); //습도 출력
+  
+  // 토양 수분 센서 (현재 -로 측정이 되는데 문제 확인 필요)
+  int groundMoisture = analogRead(PIN);  
+  groundMoisture = map(groundMoisture,550,0,0,100);  
+  Serial.print("Mositure : ");  
+  Serial.print(groundMoisture);  
+  Serial.println("%");
+
+  // 수위 측정 센서 (물 감지시 300~500 사이, 물이 없다면 ~30 -> 범위는 100을 기준으로 두면 될듯)
+  int waterTank = analogRead(PIN);
+  Serial.print("waterTank : ");
+  Serial.println(waterTank);
+
+
+  delay(5000);
 }
