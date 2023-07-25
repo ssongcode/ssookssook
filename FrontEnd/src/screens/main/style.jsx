9ec432f4c6321b12576959db052853ba7fe6d6cc
Ssuhyeong <tngud124@gmail.com>
2023-07-25 13:59:06 +0900

    [S90P12B102-192] feat : delete & regist modal 구현 및 pot & alarm Screens 구현

---
+++ FrontEnd/src/screens/main/style.jsx
@@ -0,0 +1,89 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../constants/theme";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingHorizontal: 20,
+    marginVertical: 40,
+  },
+  characterSection: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  nameTagSection: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  wateringCanSection: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  SensorContainer: {
+    flexDirection: "row",
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  IconContainer: {
+    flexDirection: "row",
+    marginVertical: 5,
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  tmp: {
+    flexDirection: "row",
+    color: COLORS.brown,
+    backgroundColor: COLORS.lightYellow,
+    borderRadius: 12.5,
+    paddingHorizontal: 7.5,
+    paddingVertical: 10,
+    textAlign: "center",
+    textAlignVertical: "center",
+    borderWidth: 2,
+    borderColor: "#896A50",
+    marginLeft: 10,
+  },
+  tmpText: {
+    fontSize: 11,
+  },
+  sensorSize: {
+    width: 15,
+    height: 15,
+    marginRight: 5,
+  },
+  mapSize: {
+    width: 50,
+    height: 50,
+    marginLeft: 15,
+  },
+  iconSize: {
+    width: 24,
+    height: 24,
+  },
+  iconBackground: {
+    backgroundColor: COLORS.lightYellow,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: COLORS.lightBrown,
+    marginLeft: 7.5,
+  },
+  characterSize: {
+    width: 400,
+    height: 400,
+  },
+  nameTagSize: {
+    width: 150,
+    height: 150,
+  },
+  wateringCanSize: {
+    width: 93,
+    height: 84,
+    marginRight: 20,
+  },
+});
+
+export default styles;
