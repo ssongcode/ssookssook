ba82893390fae2c5aa14d510e0600f60d2684e15
홍지민 <baker0929@naver.com>
2023-07-26 09:11:56 +0900

    [S09P12B102-128] feat : Dockerfile 업로드, 테스트용 세팅 업로드 (추후 삭제)

--- BE/ssuk/src/main/java/com/ssafy/ssuk/SsukApplication.java
+++ BE/ssuk/src/main/java/com/ssafy/ssuk/SsukApplication.java
@@ -2,12 +2,19 @@ package com.ssafy.ssuk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // 테스트용
 public class SsukApplication {
+    @RequestMapping("/")
+    String home() {
+        return "hi, im server!";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SsukApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SsukApplication.class, args);
+    }
 
 }
