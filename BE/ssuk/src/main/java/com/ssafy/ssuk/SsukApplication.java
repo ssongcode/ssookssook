4ba8de6cba93c9841905a4991bf9bb5a673a20bc
이덕용 <leedeok1996@naver.com>
2023-07-26 15:48:03 +0900

    feat : server

--- BE/ssuk/src/main/java/com/ssafy/ssuk/SsukApplication.java
+++ BE/ssuk/src/main/java/com/ssafy/ssuk/SsukApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SsukApplication {
     @RequestMapping("/")
     String home() {
-        return "hi, im server!";
+        return "hi, we are server!";
     }
 
     public static void main(String[] args) {
