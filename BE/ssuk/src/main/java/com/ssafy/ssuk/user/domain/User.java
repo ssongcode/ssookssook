a36596d003af9e9b67419c6325be4ac15934bad4
이덕용 <leedeok1996@naver.com>
2023-07-26 14:57:52 +0900

    feat : 빌드용 테스트

--- BE/ssuk/src/main/java/com/ssafy/ssuk/user/domain/User.java
+++ BE/ssuk/src/main/java/com/ssafy/ssuk/user/domain/User.java
@@ -2,14 +2,16 @@ package com.ssafy.ssuk.user.domain;
 
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
 
-@Entity
-@Getter
+
+// 누나 미안해 잠글게..(덕용, 지민)
+//@Entity
+//@Getter
 public class User {
 
-    @Id
+    //@Id
     private Integer id;
     private String name;
 }
