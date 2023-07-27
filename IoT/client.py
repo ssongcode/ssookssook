654a21daf6bc279ee555c9da0230b36f79fbecc0
dark6ro <dark6ro@naver.com>
2023-07-27 10:03:07 +0900

    feat. Tensorflow : 텐서플로우 세팅 완료

--- IoT/client.py
+++ IoT/client.py
@@ -4,8 +4,9 @@ import json
 import serial
 import requests
 import cv2
-import os
-import socket
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
+import numpy as np
 from datetime import datetime
 
 PORT = 'COM5' # 라즈베리 파이 PORT의 경우 확인 필요
@@ -52,6 +53,8 @@ ARD = serial.Serial(PORT, BaudRate) # 아두이노 통신 설정 - PC
 #         # Convert the JSON message to a string and send it as the STOMP SEND frame
 #         send_frame = f"SEND\ndestination:{destination}\ncontent-type:application/json\n\n{json.dumps(json_message)}\x00"
 #         await websocket.send(send_frame.encode())
+
+# Arduino Sensor Value 시리얼 통신
 def read():
 	if ARD.readable():
 		line = ARD.readline()
@@ -63,6 +66,54 @@ def read():
 	else:
 		print("Read Failed!!")
 
+# Teachable Machine 작동 로직
+def TM():
+	# Disable scientific notation for clarity
+	np.set_printoptions(suppress=True)
+	# Load the model
+	model = load_model("keras_Model.h5", compile=False)
+	# Load the labels
+	class_names = open("labels.txt", "r").readlines()
+
+	# Create the array of the right shape to feed into the keras model
+	# The 'length' or number of images you can put into the array is
+	# determined by the first position in the shape tuple, in this case 1
+	data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+	serial_number = ""
+	with open("serial_number.txt","r") as file:
+		serial_number = file.readline()
+		if serial_number == "":
+			print("시리얼 넘버가 없습니다.")
+			conn.send(-1);
+			return
+
+	filename = "PLANT_"+serial_number+".jpg"
+	# Replace this with the path to your image
+	image = Image.open("img/"+filename).convert("RGB")
+
+	# resizing the image to be at least 224x224 and then cropping from the center
+	size = (224, 224)
+	image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+	# turn the image into a numpy array
+	image_array = np.asarray(image)
+
+	# Normalize the image
+	normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+	# Load the image into the array
+	data[0] = normalized_image_array
+
+	# Predicts the model
+	prediction = model.predict(data)
+	index = np.argmax(prediction)
+	class_name = class_names[index]
+	confidence_score = prediction[0][index]
+	# Print prediction and confidence score
+	print(class_name)
+	print("Class:", class_name[2:], end="")
+	print("Confidence Score:", confidence_score)
+
 def send_image_to_server():
 	# 카메라 세팅
 	cam = cv2.VideoCapture(0)
@@ -73,7 +124,7 @@ def send_image_to_server():
 	# 파일명 : 시리얼 넘버 + 현재시간.jpg
 
 	serial_number = ""
-	with open("../serial_number.txt","r") as file:
+	with open("serial_number.txt","r") as file:
 		serial_number = file.readline()
 		if serial_number == "":
 			print("시리얼 넘버가 없습니다.")
@@ -85,18 +136,12 @@ def send_image_to_server():
 		print("프레임을 읽어올 수 없습니다.")
 		return
 	# 이미지 저장
-	cv2.imwrite("../python37/img/"+filename,frame)
+	cv2.imwrite("img/"+filename,frame)
 	# 카메라 종료
 	cam.release()
 	print("Capture request Complete!")
 	# TM 체크
-	plant_level = ""
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect(('127.0.0.1', 8888))
-		s.sendall("1".encode())
-		resp = s.recv(1024)
-		print(resp.decode())
-		plant_level = resp.decode()
+	TM()
 	# 서버로 전송
 	# url = "http://localhost:8080/upload" # 이미지 전송 할 uri
 	# dto = {
@@ -113,6 +158,10 @@ def send_image_to_server():
 if __name__ == "__main__":
 	# asyncio.get_event_loop().run_until_complete(send_json_message())
 	# asyncio.get_event_loop().run_until_complete(connect_and_subscribe())
+	cnt = 2
 	while True:
 		read()
-		send_image_to_server()
+		cnt += 1
+		if cnt == 3:
+			send_image_to_server()
+			cnt = 0
