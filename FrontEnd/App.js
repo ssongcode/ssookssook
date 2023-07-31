f14fa5f4b4aefb523290ceacf963919e6e420be8
cje9717@naver.com <cje9717@naver.com>
2023-07-31 12:42:47 +0900

    [S09P12B102-218] feat : 네비게이션 환경 구축

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -3,6 +3,13 @@ import IntroScreen from "./src/screens/intro";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "./src/screens/login";
 import { createStackNavigator } from "@react-navigation/stack";
+import PasswordFindScreen from "./src/screens/passwordfind";
+import PasswordMakeScreen from "./src/screens/passwordmake";
+import SignUpScreen from "./src/screens/signup";
+import SignUpNicknameScreen from "./src/screens/signupnickname";
+import SignUpPasswordScreen from "./src/screens/signuppassword";
+import MainScreen from "./src/screens/main";
+import PotScreen from "./src/screens/pot";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -20,6 +27,41 @@ export default function App() {
             options={{ headerShown: false }}
             name="Login"
             component={LoginScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="PasswordFind"
+            component={PasswordFindScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="PasswordMake"
+            component={PasswordMakeScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="SignUp"
+            component={SignUpScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="SignUpNickname"
+            component={SignUpNicknameScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="SignUpPassword"
+            component={SignUpPasswordScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Main"
+            component={MainScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Pot"
+            component={PotScreen}
           />
         </Stack.Navigator>
       </NavigationContainer>
