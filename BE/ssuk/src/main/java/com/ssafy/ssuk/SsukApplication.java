ef7f50ea0270c843beb409b4e3aa392a668a8ff6
DeokyongLee <igloo104@naver.com>
2023-08-03 02:12:44 +0900

    [S07P12B102-203] feat : 이미지 서버 생성 및 테스트

--- BE/ssuk/src/main/java/com/ssafy/ssuk/SsukApplication.java
+++ BE/ssuk/src/main/java/com/ssafy/ssuk/SsukApplication.java
@@ -7,10 +7,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+})
 @RestController // 테스트용
 @RequestMapping("/")
 public class SsukApplication {
+    public static final String APPLICATION_LOCATIONS = "spring.config.location=" +
+            "classpath:application.yml" +
+            "classpath:aws.yml";
+
     @GetMapping("")
     String home()
     {
