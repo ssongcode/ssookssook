718cb2c0577d897731e52d76008e600d8f83ad84
송소연 <songsoy95@gmail.com>
2023-08-02 14:10:05 +0900

    [S09P12B102-268] feat : JWT 토큰 구현 완료

--- BE/ssuk/src/main/java/com/ssafy/ssuk/utils/jwt/CustomUserDetailsService.java
+++ BE/ssuk/src/main/java/com/ssafy/ssuk/utils/jwt/CustomUserDetailsService.java
@@ -26,7 +26,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 //                .map(this::createUserDetails)
 //                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
     }
-    
+
     // 해당하는 User의 데이터가 존재하면 UserDetails 객체로 만들어서 리턴
 //    private UserDetails createUserDetails(User user) {
 //        org.springframework.security.core.userdetails.User.UserBuilder roles = org.springframework.security.core.userdetails.User.builder()
@@ -39,4 +39,5 @@ public class CustomUserDetailsService implements UserDetailsService {
 //
 //        return build;
 //    }
+    
 }
