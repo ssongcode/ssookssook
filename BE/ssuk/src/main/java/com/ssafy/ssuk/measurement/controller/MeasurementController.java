542be991655e51c22ed731458a555522aaafec74
HongJiMin <baker0929@naver.com>
2023-08-02 13:34:26 +0900

    [S07P12B102-274] feat : 물 급수 컨트롤러 생성

--- BE/ssuk/src/main/java/com/ssafy/ssuk/measurement/controller/MeasurementController.java
+++ BE/ssuk/src/main/java/com/ssafy/ssuk/measurement/controller/MeasurementController.java
@@ -1,25 +1,33 @@
 package com.ssafy.ssuk.measurement.controller;
 
 import com.ssafy.ssuk.measurement.domain.Measurement;
+import com.ssafy.ssuk.measurement.dto.socket.SensorMessageDto;
 import com.ssafy.ssuk.measurement.service.MeasurementService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@Slf4j
 @RequestMapping("/sensor")
 public class MeasurementController {
 
     private final MeasurementService mesurementService;
+    private final SimpMessagingTemplate template;
 
     @Autowired
-    public MeasurementController(MeasurementService mesurementService) {
+    public MeasurementController(MeasurementService mesurementService, SimpMessagingTemplate template) {
         this.mesurementService = mesurementService;
+        this.template = template;
     }
 
     @GetMapping("/{user_id}/{pot_id}")
@@ -28,4 +36,21 @@ public class MeasurementController {
 
         return ResponseEntity.ok(result);
     }
+
+//    @GetMapping("/water/{potId}")
+//    ResponseEntity<?> test(@RequestAttribute Integer userId, @PathVariable Integer potId)
+//    {
+//        return ResponseEntity.ok("hi");
+//    }
+
+    @GetMapping("/water/{potId}")
+    void test(@PathVariable String potId)
+    {
+        SensorMessageDto sensorMessageDto = new SensorMessageDto();
+        sensorMessageDto.setMeasurementValue(2.3);
+        sensorMessageDto.setPotId(1);
+        sensorMessageDto.setSerialNumber("11111111");
+        template.convertAndSend("/sub/socket/room/" + "11111111", sensorMessageDto);
+    }
+
 }
