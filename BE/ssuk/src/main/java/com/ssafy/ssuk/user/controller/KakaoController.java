6629a3d5eff1073a4127fa49ea2d8067f05ab91b
이덕용 <leedeok1996@naver.com>
2023-08-10 10:52:49 +0900

    [S09P12B102-326] test : kakao 테스트

--- BE/ssuk/src/main/java/com/ssafy/ssuk/user/controller/KakaoController.java
+++ BE/ssuk/src/main/java/com/ssafy/ssuk/user/controller/KakaoController.java
@@ -1,24 +1,30 @@
 package com.ssafy.ssuk.user.controller;
 
+import com.ssafy.ssuk.user.domain.User;
+import com.ssafy.ssuk.user.dto.request.KakaoCodeRequsetDto;
+import com.ssafy.ssuk.utils.auth.jwt.TokenInfo;
 import com.ssafy.ssuk.utils.auth.oauth.kakao.KakaoAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/kakao")
 public class KakaoController {
 
     private final KakaoAuthService kakaoAuthService;
 
     private final String REDIRECT_URL = "http://i9b102.p.ssafy.io:8080/kakao";
-    @GetMapping("/kakao")
-    public String kakaoLogin(@RequestParam String code) throws Exception {
+    @GetMapping("")
+    public String kakao(@RequestParam String code) throws Exception {
         log.debug("tmpcode={}", code);
-        String kakaoAccessToken = kakaoAuthService.getAccessToken(code, REDIRECT_URL).getAccessToken();
+//        String kakaoAccessToken = kakaoAuthService.getAccessToken(code, REDIRECT_URL).getAccessToken();
 //        // 사용자 정보 가져오거나 회원가입
 //        User user = kakaoAuthService.saveOrGetUser(kakaoAccessToken);
 ////        redisService.
@@ -26,6 +32,21 @@ public class KakaoController {
 //        TokenInfo tokenInfo = kakaoAuthService.kakaoLogin(user.getEmail());
 //        log.debug("loginTokenInfo={}", tokenInfo);
 
-        return "redirect:http://i9b102.p.ssafy.io:8080";
+        return "redirect:http://i9b102.p.ssafy.io:8080?code=" + code;
+    }
+
+    @PostMapping("/code")
+    public ResponseEntity<TokenInfo> kakaoLogin(@RequestBody @Validated KakaoCodeRequsetDto kakaoCodeRequsetDto) throws Exception {
+        String code = kakaoCodeRequsetDto.getCode();
+        log.debug("code={}", code);
+        String kakaoAccessToken = kakaoAuthService.getAccessToken(code).getAccessToken();
+        // 사용자 정보 가져오거나 회원가입
+        User user = kakaoAuthService.saveOrGetUser(kakaoAccessToken);
+//        redisService.
+        log.debug("회원가입 또는 사용자 정보 가져오기");
+        TokenInfo tokenInfo = kakaoAuthService.kakaoLogin(user.getEmail());
+        log.debug("loginTokenInfo={}", tokenInfo);
+
+        return new ResponseEntity<>(tokenInfo, HttpStatus.OK);
     }
 }
